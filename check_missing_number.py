a0bd90b0d718ef1550057cd04799758b4a1cf2ea
chocomusrt <44128043+chocomusrt@users.noreply.github.com>
2018-10-18 21:37:35 -0400

    Update check_missing_number.py
    added another method to calculate difference in lists

--- check_missing_number.py
+++ check_missing_number.py
@@ -1,4 +1,6 @@
-# Create a list with a missing number 1 - 100
+# Method 1 - Create a list with a missing number 1 - 100
+# Using summationg of two sets
+########################################################
 check_list = []
 for i in range(1,100):
 	check_list.append(i)
@@ -13,4 +15,11 @@ print(full_list)
 ## compare the list and print the missing value
 answer = sum(full_list) - sum(check_list)
 
-print(answer)
+print('The missing number is : ' + str(answer))
+
+
+# Method 2 for loop and if statement
+#######################################################
+difference = [i for i in check_list + full_list if i not in check_list or i not in full_list]
+print(difference)
+        
